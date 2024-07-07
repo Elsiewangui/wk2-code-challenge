2f8677ca71455d3f8e83d96be15aaf9a903130e9
Wangui <elswangui@gmail.com>
2024-07-07 22:13:15 +0300

    updates index.html

---
+++ index.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+const addItemButton = document.querySelector('.add-btn');
+const clearItemButton = document.querySelector('.clear-btn');
+const markPurchasedButton=document.querySelector('.purchased-btn')
+const itemList = document.getElementById('item-List');
+const itemInput = document.getElementById('item');
+  
+function addItem(newItem) {
+    const li = document.createElement('li');
+    li.textContent = newItem;
+    itemList.appendChild(li);
+    itemInput.value = '';
+    li.addEventListener('click', () => {
+        li.classList('purchased');
+       
+    });
+}
+
+addItemButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    const newItem = itemInput.value
+    if (newItem !== '') {
+        addItem(newItem);
+    }
+})
+markPurchasedButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    const items = itemList.querySelectorAll('li');
+    items.forEach(item => {
+      if (!item.classList.contains('purchased')) {
+        item.classList.add('purchased');
+      }
+    });
+  });
+
+  
+clearItemButton.addEventListener('click', function(event) {
+     event.preventDefault();
+      itemList.innerHTML = '';
+      
+    });
+})
